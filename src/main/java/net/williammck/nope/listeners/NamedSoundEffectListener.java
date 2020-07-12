64f438df85334d0410bfe5d8157daf93757a3f29
William McKinnerney <git@williammck.net>
2020-07-12 11:59:25 -0500

    Use Sound enum and regex match enderman sounds/footsteps

--- src/main/java/net/williammck/nope/listeners/NamedSoundEffectListener.java
+++ src/main/java/net/williammck/nope/listeners/NamedSoundEffectListener.java
@@ -6,6 +6,7 @@ import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import net.williammck.nope.Util;
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -21,9 +22,9 @@ public class NamedSoundEffectListener extends PacketAdapter {
     @Override
     public void onPacketSending(PacketEvent event) {
         PacketContainer packet = event.getPacket();
-        String soundId = packet.getStrings().read(0);
+        Sound soundEffect = packet.getSoundEffects().read(0);
 
-        if (soundId.startsWith("mob.enderman.")) {
+        if (soundEffect.name().matches("(ENTITY_(HOSTILE|ENDERMAN)_[A-Z_]+|BLOCK_[A-Z_]+_(STEP|FALL))")) {
             // Determine player "nope eligibility"
             Player player = event.getPlayer();
             Location packetLoc = getPacketLocation(packet, player.getWorld());
